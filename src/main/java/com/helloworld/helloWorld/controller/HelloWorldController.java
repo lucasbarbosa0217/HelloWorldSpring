3ffd9cbf8ccc29c3eb8e8f65b2a056e5835b8552
Lucas Barbosa <lucasbarbosa0217@gmail.com>
2024-07-01 13:55:35 -0300

    Primeira atividade

--- src/main/java/com/helloworld/helloWorld/controller/HelloWorldController.java
+++ src/main/java/com/helloworld/helloWorld/controller/HelloWorldController.java
@@ -1,16 +1,38 @@
 package com.helloworld.helloWorld.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello-world")
+@RequestMapping("/")
 public class HelloWorldController {
-	
-	@GetMapping
+
+	ArrayList<String> bsmList = new ArrayList<String>(
+			Arrays.asList("Orientação ao Futuro", "Responsabilidade Pessoal", "Mentalidade de Crescimento",
+					"Persistência", "Trabalho em Equipe", "Atenção aos Detalhes", "Proatividade", "Comunicação"));
+
+	ArrayList<String> objectiveList = new ArrayList<String>(
+			Arrays.asList("Compreender mais aprofundadamente o conceito RestFul", "Criar um crud com o Spring",
+					"Adiantar alguns detalhes do design do projeto integrador",
+					"Ajustar meu currículo para as aulas de empregabilidade"));
+
+	@GetMapping("/hello-world")
 	public String helloWorld() {
-		System.out.println("teste");
 		return "Hello World";
 	}
+
+	@GetMapping("/bsm")
+	public ArrayList<String> bsm() {
+		return bsmList;
+	}
+
+	@GetMapping("/objectives")
+	public ArrayList<String> objetivos() {
+		return objectiveList;
+	}
+
 }
